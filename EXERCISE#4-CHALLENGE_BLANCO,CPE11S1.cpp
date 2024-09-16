1b8f33684f7cfeee8d744ccd548019ae2f54256b
jamil-blanco <qjmcblanco@tip.edu.ph>
2024-09-16 12:35:35 +0800

    Add files via upload

---
+++ EXERCISE#4-CHALLENGE_BLANCO,CPE11S1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	int i=4;
+	
+	while (i<=9)
+	
+	
+	{
+		cout<<i<<" is the interger that are i<10 but i>3 "<<endl;
+		i=i+1;
+	}
+	
+	return 0;
+}
