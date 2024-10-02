668e72dcd2d7cf09e5d05b12c605af286c69e958
jamil-blanco <qjmcblanco@tip.edu.ph>
2024-10-02 12:46:29 +0800

    Add files via upload

---
+++ PRACTICAL-LAB#2_BLANCO.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+	// Input
+	
+	int num1, num2, num3, num4, num5, sum;
+	
+	cout<<" Input 1st number = ";
+	cin>>num1;
+	cout<<" Input 2nd number = ";
+	cin>>num2;
+	cout<<" Input 3rd number = ";
+	cin>>num3;
+	cout<<" Input 4th number = ";
+	cin>>num4;
+	cout<<" Input 5th number = ";
+	cin>>num5;
+	
+	// Computation of Sum
+	
+	sum=num1+num5;
+	
+	cout<<"The sum of the 1st number and 5th number is " <<sum<<endl;
+	
+	return 0;
+	
+}
