e84c99e46fc05bdfbba94f988df039bff59f433e
jamil-blanco <qjmcblanco@tip.edu.ph>
2024-10-14 11:27:31 +0800

    Add files via upload

---
+++ Hands-on Activity 3.2: Program Control/ACTIVITY7_1_BALNCO.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	// Input
+	int sum = 0;
+	int number;
+	
+	// Process
+	for (number = 2; number <= 100; number += 2)
+	{
+		sum += number;
+	}
+	
+	// Output
+	printf("Sum:%d\n", sum);
+	
+	return 0;
+}
