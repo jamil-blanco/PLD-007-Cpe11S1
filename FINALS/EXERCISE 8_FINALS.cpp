ea57b320c39f4083f7ee07c71f7e61f977112246
jamil-blanco <qjmcblanco@tip.edu.ph>
2024-11-25 11:42:23 +0800

    Add files via upload

---
+++ FINALS/EXERCISE 8_FINALS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{ 
+	// Simple function
+	int num;
+	cout << "Enter a number: ";
+	cin >> num;
+	cout << "You entered: " << num << endl;
+	return 0;
+	
+}
